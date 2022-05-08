18c1ae5a3865a10a290f887d69423318f7fc6b13
Николай Стригачев <nikolai7832@yandex.ru>
2022-05-08 23:27:31 +0300

    Добавьте файлы проекта.

---
+++ CollectionMod13/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace Collections
+{
+    using System.Diagnostics;
+
+    class Programm
+    {
+        static void Main()
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Text\Text1.txt");
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                List<string> DefaultList = new List<string>();
+
+                Console.WriteLine("Обычный список\n");
+
+                Write(DefaultList, sr);
+                Read(DefaultList);
+                Console.WriteLine();
+
+                LinkedList<string> LinkList = new LinkedList<string>();
+
+                Console.WriteLine("Связанный список\n");
+                Write(LinkList, sr);
+                Read(LinkList);
+                Console.Read();
+            }
+        }
+
+        // Запись в файл
+        #region Write
+
+        static void Write(List<string> list, StreamReader sr)
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            while (!sr.EndOfStream)
+            {
+                list.Add(sr.ReadLine());
+            }
+
+            Console.WriteLine($"Время записи: {stopWatch.Elapsed.TotalMilliseconds}");
+        }
+
+        static void Write(LinkedList<string> list, StreamReader sr)
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            while (!sr.EndOfStream)
+            {
+                list.AddLast(sr.ReadLine());
+            }
+
+            Console.WriteLine($"Время записи: {stopWatch.Elapsed.TotalMilliseconds}");
+        }
+        #endregion
+
+        // Чтение из файла
+        #region Read
+        static void Read(List<string> list)
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            foreach (var item in list)
+            {
+                string str = item;
+            }
+
+            Console.WriteLine($"Время чтения: {stopWatch.Elapsed.TotalMilliseconds}");
+        }
+
+        static void Read(LinkedList<string> list)
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            foreach (var item in list)
+            {
+                string str = item;
+            }
+
+            Console.WriteLine($"Время чтения: {stopWatch.Elapsed.TotalMilliseconds}");
+        }
+        #endregion
+    }
+}
---
+++ Counter/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Collections
+{
+    using System.Linq;
+    using System.Globalization;
+    class Programm
+    {
+        static char[] delimeters = Enumerable.Range(0, 256) // Здесь храним разделители (среднне тире сюда не попадает,даже если прописывать в ручную)
+                            .Select(x => (char)x)
+                            .Where(c => char.IsWhiteSpace(c) || char.IsPunctuation(c) || char.IsSurrogate(c) || char.IsDashPunctuation)
+                            .ToArray();
+
+        static void Main()
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Text\Text1.txt");
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                var text = sr.ReadToEnd().ToLower().Split(delimeters, StringSplitOptions.RemoveEmptyEntries); // В массиве хранится текст разделенный по словам
+
+                Dictionary<string, int> words = text
+                    .GroupBy(s => s)
+                    .ToDictionary(g => g.Key, g => g.Count()); // Собираем слова в пары слово-колличетсво
+
+                words = words.OrderByDescending (x => x.Value)
+                       .ToDictionary(x => x.Key, x => x.Value); // Сортируем от большего к меньшему
+
+                words.Remove("\u2013");  // Удаляем среднее тире
+
+                words = words.Take(10)
+                       .ToDictionary(g => g.Key, g => g.Value); // Берем первые 10
+
+                foreach (var word in words)
+                    Console.WriteLine(word);
+            }
+        }
+    }
+}
